acbd88f4fa25cdb27732c50af7bb073842eb04ee
Vignesh <vigneshb6789@gmail.com>
2018-02-19 00:34:56 -0600

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,14 @@
 var mongoose = require('mongoose');
 
+const REMOTE_MONGO = 'mongodb://Vignesh:cruds@ds241578.mlab.com:41578/cruds';
+const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
+const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(MONGO_URI).then(() => {
+console.log('Connected to Mongo instance.')
+}, (err) => {
+   console.log('Error connecting to Mongo instance: ', err);
+  });
 
 module.exports = {mongoose};
